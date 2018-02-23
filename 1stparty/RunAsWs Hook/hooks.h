6d7139190e2c560ce0b90367d38316fe69bc72c9
Martin <Martin@80b7e1f2-6439-1f49-8eee-53227df04c3f>
2018-02-23 14:19:57 +0000

    * using

--- 1stparty/RunAsWs Hook/hooks.h
+++ 1stparty/RunAsWs Hook/hooks.h
@@ -1,11 +1,11 @@
 #pragma once
 
-typedef BOOL (WINAPI *CreateProcessAType)  (LPCSTR lpApplicationName, LPSTR lpCommandLine, LPSECURITY_ATTRIBUTES lpProcessAttributes, LPSECURITY_ATTRIBUTES lpThreadAttributes, BOOL bInheritHandles, DWORD dwCreationFlags, LPVOID lpEnvironment, LPCTSTR lpCurrentDirectory, LPSTARTUPINFO lpStartupInfo, LPPROCESS_INFORMATION lpProcessInformation);
-typedef BOOL (WINAPI *CreateProcessWType)  (LPCWSTR lpApplicationName, LPWSTR lpCommandLine, LPSECURITY_ATTRIBUTES lpProcessAttributes, LPSECURITY_ATTRIBUTES lpThreadAttributes, BOOL bInheritHandles, DWORD dwCreationFlags, LPVOID lpEnvironment, LPCTSTR lpCurrentDirectory, LPSTARTUPINFO lpStartupInfo, LPPROCESS_INFORMATION lpProcessInformation);
-typedef BOOL (WINAPI *GetVersionExAType)   (LPOSVERSIONINFO lpVersionInfo);
-typedef BOOL (WINAPI *GetVersionExWType)   (LPOSVERSIONINFO lpVersionInfo);
-typedef LONG (WINAPI *RegQueryValueExAType)(HKEY hKey, LPCSTR lpValueName, LPDWORD lpReserved, LPDWORD lpType, LPBYTE lpData, LPDWORD lpcbData);
-typedef LONG (WINAPI *RegQueryValueExWType)(HKEY hKey, LPCWSTR lpValueName, LPDWORD lpReserved, LPDWORD lpType, LPBYTE lpData, LPDWORD lpcbData);
+using CreateProcessAType   = BOOL(WINAPI *)(LPCSTR lpApplicationName, LPSTR lpCommandLine, LPSECURITY_ATTRIBUTES lpProcessAttributes, LPSECURITY_ATTRIBUTES lpThreadAttributes, BOOL bInheritHandles, DWORD dwCreationFlags, LPVOID lpEnvironment, LPCTSTR lpCurrentDirectory, LPSTARTUPINFO lpStartupInfo, LPPROCESS_INFORMATION lpProcessInformation);
+using CreateProcessWType   = BOOL(WINAPI *)(LPCWSTR lpApplicationName, LPWSTR lpCommandLine, LPSECURITY_ATTRIBUTES lpProcessAttributes, LPSECURITY_ATTRIBUTES lpThreadAttributes, BOOL bInheritHandles, DWORD dwCreationFlags, LPVOID lpEnvironment, LPCTSTR lpCurrentDirectory, LPSTARTUPINFO lpStartupInfo, LPPROCESS_INFORMATION lpProcessInformation);
+using GetVersionExAType    = BOOL(WINAPI *)(LPOSVERSIONINFO lpVersionInfo);
+using GetVersionExWType    = BOOL(WINAPI *)(LPOSVERSIONINFO lpVersionInfo);
+using RegQueryValueExAType = LONG(WINAPI *)(HKEY hKey, LPCSTR lpValueName, LPDWORD lpReserved, LPDWORD lpType, LPBYTE lpData, LPDWORD lpcbData);
+using RegQueryValueExWType = LONG(WINAPI *)(HKEY hKey, LPCWSTR lpValueName, LPDWORD lpReserved, LPDWORD lpType, LPBYTE lpData, LPDWORD lpcbData);
 
 BOOL WINAPI hook_CreateProcessA  (LPCSTR lpApplicationName, LPSTR lpCommandLine, LPSECURITY_ATTRIBUTES lpProcessAttributes, LPSECURITY_ATTRIBUTES lpThreadAttributes, BOOL bInheritHandles, DWORD dwCreationFlags, LPVOID lpEnvironment, LPCTSTR lpCurrentDirectory, LPSTARTUPINFO lpStartupInfo, LPPROCESS_INFORMATION lpProcessInformation);
 BOOL WINAPI hook_CreateProcessW  (LPCWSTR lpApplicationName, LPWSTR lpCommandLine, LPSECURITY_ATTRIBUTES lpProcessAttributes, LPSECURITY_ATTRIBUTES lpThreadAttributes, BOOL bInheritHandles, DWORD dwCreationFlags, LPVOID lpEnvironment, LPCTSTR lpCurrentDirectory, LPSTARTUPINFO lpStartupInfo, LPPROCESS_INFORMATION lpProcessInformation);
@@ -14,9 +14,9 @@ BOOL WINAPI hook_GetVersionExW   (LPOSVERSIONINFO lpVersionInfo);
 LONG WINAPI hook_RegQueryValueExA(HKEY hKey, LPCSTR lpValueName, LPDWORD lpReserved, LPDWORD lpType, LPBYTE lpData, LPDWORD lpcbData);
 LONG WINAPI hook_RegQueryValueExW(HKEY hKey, LPCWSTR lpValueName, LPDWORD lpReserved, LPDWORD lpType, LPBYTE lpData, LPDWORD lpcbData);
 
-extern CreateProcessAType g_createProcessA;
-extern CreateProcessWType g_createProcessW;
-extern GetVersionExAType g_getVersionExA;
-extern GetVersionExWType g_getVersionExW;
+extern CreateProcessAType   g_createProcessA;
+extern CreateProcessWType   g_createProcessW;
+extern GetVersionExAType    g_getVersionExA;
+extern GetVersionExWType    g_getVersionExW;
 extern RegQueryValueExAType g_regQueryValueExA;
 extern RegQueryValueExWType g_regQueryValueExW;
