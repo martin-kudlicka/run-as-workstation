f4f329d8d6d97f1130184feb7dd5017207f245eb
martin-kudlicka <martink-dev@yandex.com>
2021-02-07 14:51:46 +0100

    * 1.0.16

--- 1stparty/RunAsWs Hook/dllmain.cpp
+++ 1stparty/RunAsWs Hook/dllmain.cpp
@@ -7,19 +7,19 @@ HMODULE g_module = nullptr;
 
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved)
 {
-  UNREFERENCED_PARAMETER(lpReserved);
+	UNREFERENCED_PARAMETER(lpReserved);
 
 	switch (ul_reason_for_call)
 	{
-	  case DLL_PROCESS_ATTACH:
-      g_module = hModule;
-      hookNow();
-      _beginthreadex(nullptr, 0, &hookThread, nullptr, 0, nullptr);
-      break;
-	  case DLL_PROCESS_DETACH:
-      uninstallHooks();
-      g_module = nullptr;
-		  break;
+		case DLL_PROCESS_ATTACH:
+			g_module = hModule;
+			hookNow();
+			_beginthreadex(nullptr, 0, &hookThread, nullptr, 0, nullptr);
+			break;
+		case DLL_PROCESS_DETACH:
+			uninstallHooks();
+			g_module = nullptr;
+			break;
 	}
 
 	return TRUE;
